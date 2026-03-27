0fee3a614b96c34500268bcc9660dd03f12f3a33
brittaney <serena96688@gmail.com>
2026-03-26 23:52:25 -0400

    Fix Makefile, protocol payloads, select loop, and style issues

--- Makefile
+++ Makefile
@@ -1,17 +1,21 @@
+
 FLAGS = -Wall -Wextra -g
 CC = gcc
 
-PORT ?= 4242
+PORT = 4242
 
-all: server worker
+all: server worker gen_data
 
 server: server.c net_utils.c io_utils.c model.c protocol.h net_utils.h io_utils.h model.h
-	$(CC) $(FLAGS) -DPORT=$(PORT) -o server server.c net_utils.c io_utils.c model.c
+	$(CC) $(FLAGS) -DPORT=$(PORT) -o server server.c net_utils.c io_utils.c model.c -lm
 
 worker: worker.c net_utils.c io_utils.c model.c data.c protocol.h net_utils.h io_utils.h model.h data.h
-	$(CC) $(FLAGS) -DPORT=$(PORT) -o worker worker.c net_utils.c io_utils.c model.c data.c
+	$(CC) $(FLAGS) -DPORT=$(PORT) -o worker worker.c net_utils.c io_utils.c model.c data.c -lm
+
+gen_data: gen_data.c
+	$(CC) $(FLAGS) -o gen_data gen_data.c -lm
 
 clean:
-	rm -f server worker *.o
+	rm -f server worker gen_data *.o
 
 .PHONY: all clean
--- net_utils.c
+++ net_utils.c
@@ -4,61 +4,62 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include "net_utils.h"
-
 #include <arpa/inet.h>
+#include <netdb.h>
 #include <errno.h>
 
+#include "net_utils.h"
+
 void init_server_addr(struct sockaddr_in *addr, int port) {
-	memset(addr, 0, sizeof(*addr));
-	addr->sin_family = AF_INET;
-	addr->sin_addr.s_addr = htonl(INADDR_ANY);
-	addr->sin_port = htons(port);
+    memset(addr, 0, sizeof(*addr));
+    addr->sin_family = AF_INET;
+    addr->sin_addr.s_addr = htonl(INADDR_ANY);
+    addr->sin_port = htons(port);
 }
 
 
 int set_up_server_socket(int port) {
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) {
-		perror("socket");
-		return -1;
-	}
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("socket");
+        return -1;
+    }
 
-	int opt = 1;
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-		perror("setsockopt");
-		close(sockfd);
-		return -1;
-	}
+    int opt = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("setsockopt");
+        close(sockfd);
+        return -1;
+    }
 
-	struct sockaddr_in serv_addr;
-	init_server_addr(&serv_addr, port);
+    struct sockaddr_in serv_addr;
+    init_server_addr(&serv_addr, port);
 
-	if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-		perror("bind");
-		close(sockfd);
-		return -1;
-	}
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("bind");
+        close(sockfd);
+        return -1;
+    }
 
-	if (listen(sockfd, 16) < 0) {
-		perror("listen");
-		close(sockfd);
-		return -1;
-	}
+    if (listen(sockfd, 16) < 0) {
+        perror("listen");
+        close(sockfd);
+        return -1;
+    }
 
-	return sockfd;
+    return sockfd;
 }
 
 
 int accept_connection(int server_fd) {
-	struct sockaddr_in cli_addr;
-	socklen_t cli_len = sizeof(cli_addr);
-	int client_fd = accept(server_fd, (struct sockaddr *)&cli_addr, &cli_len);
-	if (client_fd < 0) {
-		perror("accept");
-		return -1;
-	}
-	return client_fd;
+    struct sockaddr_in cli_addr;
+    socklen_t cli_len = sizeof(cli_addr);
+    int client_fd = accept(server_fd, (struct sockaddr *)&cli_addr, &cli_len);
+    if (client_fd < 0) {
+        perror("accept");
+        return -1;
+    }
+    return client_fd;
 }
 
 
@@ -75,11 +76,14 @@ int connect_to_server(const char *host, int port) {
 	serv_addr.sin_port = htons(port);
 
 	if (inet_pton(AF_INET, host, &serv_addr.sin_addr) <= 0) {
-		/* try localhost */
-		if (strcmp(host, "localhost") == 0) {
+		/* Not a numeric IP -- try DNS lookup as a fallback */
+		struct hostent *h = gethostbyname(host);
+		if (h && h->h_addr_list && h->h_addr_list[0]) {
+			memcpy(&serv_addr.sin_addr.s_addr, h->h_addr_list[0], h->h_length);
+		} else if (strcmp(host, "localhost") == 0) {
 			serv_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 		} else {
-			fprintf(stderr, "invalid host address: %s\n", host);
+			fprintf(stderr, "invalid host/address or DNS lookup failed: %s\n", host);
 			close(sockfd);
 			return -1;
 		}
